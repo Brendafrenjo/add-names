5d08af18c004b270eaba1773ccb708cbb5e4a7ab
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2023-10-01 22:54:55 +0300

    Updated reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,13 @@
 const initialState = {
   names: [],
-};
+}
 
 function namesReducer(state = initialState, action) {
   switch (action.type) {
-    case "ADD_NAME":
+    case "ADD_NAME": 
       return {
         ...state,
-        names: [...state.names, action.payload],
-      };
-    default:
-      return state;
+        names: [...state.names, action.payload]
+      }
+    }
   }
-}
-
-export default namesReducer
